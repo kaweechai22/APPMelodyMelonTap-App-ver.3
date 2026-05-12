011fb9e8aa4bc5572fe4ae0878fde3752b329dbf
kaweechai22 <cacarosjampa@gmail.com>
2026-05-12 20:55:17 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="melon-clean-stable-v19-1-audiofix";
+const CACHE_NAME="melon-clean-stable-v19-2-naturalpause";
 const ASSETS=["./","./index.html","./manifest.json","./icon.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))).then(()=>self.clients.claim()));});
